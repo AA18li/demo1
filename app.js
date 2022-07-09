9c2676786cf109e0b670c3e0a2c3433cb185e226
AA18li <aytennadirli14+done1@gmail.com>
2022-07-09 11:21:37 +0000

    about

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 "use strict";
 $(document).ready(function () {
 
+
+    $('h1').click(function (e) {
+        e.preventDefault();
+        $('h1').html(1234);
+    } );
+
+
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = (a) =>  {
+            for ( let i = 1; i <=a; i++) {
+                $('.col-md-12').append(`<h2>${i}</h2>`);
+            }
+        }
+    })
+    a($('input').val())
 });
+
+console.log($('input').val());
+
