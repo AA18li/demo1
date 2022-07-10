4e86d397eb48f464b30838b1fab5321bcf402ee7
AA18li <aytennadirli14+done1@gmail.com>
2022-07-10 16:09:53 +0000

    about

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
+// "use strict";
+// $(document).ready(function () {
+
+
+//     $('.btn').click(function (e) {
+//         e.preventDefault();
+
+//         let a = (a) =>  {
+//             for ( let i = 1; i <=a; i++) {
+//                 $('.col-md-12').append(`<h2>${i}</h2>`);
+//             }
+//         }
+//     })
+//     a($('input').val())
+// });
+
 "use strict";
 $(document).ready(function () {
 
-
-    $('h1').click(function (e) {
+    $('#btn').click(function (e) {
         e.preventDefault();
-        $('h1').html(1234);
-    } );
-
-
-    $('.btn').click(function (e) {
-        e.preventDefault();
-
-        let a = (a) =>  {
-            for ( let i = 1; i <=a; i++) {
-                $('.col-md-12').append(`<h2>${i}</h2>`);
+        let func = a => {
+            if (Number(a)) {
+                for (let i = 1; i <= a; i++) {
+                    $('.btn').append(`<h1>${a}</h1>`);
+                }
+            } else {
+                $('.contentDiv').append(`<h1>${a}</h1>`);
             }
         }
-    })
-    a($('input').val())
+        func($('input').val());
+    });
+
 });
-
-console.log($('input').val());
-
