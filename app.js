708d697abf298cf754909c2935e1303383840fda
AA18li <aytennadirli14+done1@gmail.com>
2022-07-13 14:38:10 +0000

    about

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ $(document).ready(function () {
         if ($.trim($('input').val()) == '') {
             $('input').val();
         } else {
-            
+            let k = v => {
+                for (let i = 0; i < v; i++) { $('.minibox').append(`<h3>${v}</h3>`) };
+            }
+            let z = e => Number(e) ? k(e) : $('.minibox').append(`<h3>${e}</h3>`);
+            z($('input').val());
         }
         $('input').val('') 
     });
