fe1f80a2a4573f97ae7d5af1c022c7c73bc948ca
AA18li <aytennadirli14+done1@gmail.com>
2022-07-13 14:38:02 +0000

    about

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 "use strict";
 $(document).ready(function () {
-    
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if ($.trim($('input').val()) == '') {
+            $('input').val();
+        } else {
+            
+        }
+        $('input').val('') 
+    });
 });
